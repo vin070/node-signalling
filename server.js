cff99b630503339e6df971b9801b26ca730ab01e
vineet kumar <vineet.kumar@startelelogic.co.in>
2022-08-27 19:01:51 +0530

    code uypdated

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
-let app = require('express')();
-let {Server} = require("socket.io");
+let app = require("express")();
+let { Server } = require("socket.io");
 let fs = require("fs");
 let https = require("https");
-let {config} = require('./env/env');
+let { config } = require("./env/env");
 
 //certificate path
 let certificates = {
-    cert:fs.readFileSync('./assets/keys/fullchain.pem'),
-    key:fs.readFileSync('./assets/keys/privkey.pem')
+  cert: fs.readFileSync("./assets/keys/fullchain.pem"),
+  key: fs.readFileSync("./assets/keys/privkey.pem"),
 };
 
 //start https server and listen on it
-let server = https.createServer(certificates,app).listen(config.PORT)
+let server = https.createServer(certificates, app).listen(config.PORT);
 
 // create socket.io and bind socket.io to this server
-let io = new Server(server,{
-    cors:{
-        origin:"*"
-    }
+let io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
 //routing events listen
-app.get('/',function(req,res,next){
-    res.send("welcome to signalling server");
-    res.end();
+app.get("/", function (req, res, next) {
+  res.send("welcome to signalling server");
+  res.end();
 });
 
 //socket.io events
-let connectedUser=[]
+let connectedUser = [];
 
-io.on("connection",function(socket){
-      
-     socket.on("join",({email,room_name})=>{
-         connectedUser.push({email:email,socketId:socket.id});
-         socket.join(room_name);
-         socket.to(room_name).emit("room_join",{"email":email});    
-     });
+io.on("connection", function (socket) {
+  socket.on("join", ({ email, room_name }) => {
+    connectedUser.push({ email: email, socketId: socket.id });
+    socket.join(room_name);
+    socket.to(room_name).emit("room_join", { email: email });
+  });
 
-     socket.on("message",({room_name,data})=>{
-         socket.to(room_name).emit("message",data); 
-     });
+  socket.on("message", ({ room_name, data }) => {
+    socket.to(room_name).emit("message", data);
+  });
 
-     socket.on("left",({room_name})=>{
-         socket.leave(room_name);
-     });
-     
+  socket.on("left", ({ room_name }) => {
+    socket.leave(room_name);
+  });
 });
